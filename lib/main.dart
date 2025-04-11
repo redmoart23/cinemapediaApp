dabe58fc9c0400cfb6b48acb2473d01347c76454
red <redmoart@gmail.com>
2025-04-11 16:49:49 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'config/theme/app_theme.dart';
+
+void main() {
+  runApp(const CinemapediaApp());
+}
+
+class CinemapediaApp extends StatelessWidget {
+  const CinemapediaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
